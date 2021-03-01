a397863b8898c7c14f3a9d99056175d98da9d83a
ShianWang <984292420@qq.com>
2021-03-01 14:26:33 +0800

    feat: change main style

--- src/modules/sale/App.svelte
+++ src/modules/sale/App.svelte
@@ -7,6 +7,12 @@
   import routes from './routes'
 </script>
 
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
 <h1>svelte-spa-router sample</h1>
 <h2>Dynamic imports</h2>
 

c3569b9bb6982343daff7c12068d0279c5e6b5f6
ShianWang <984292420@qq.com>
2021-03-08 22:48:28 +0800

    feat: add domUtils

---
+++ src/utils/dom.ts
@@ -0,0 +1,173 @@
+const SPECIAL_CHARS_REGEXP = /([:-_]+(.))/g;
+const MOZ_HACK_REGEXP = /^moz([A-Z])/;
+
+/* istanbul ignore next */
+const trim = function(string: string) {
+  return (string || '').replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, '');
+};
+/* istanbul ignore next */
+const camelCase = function(name: string) {
+  return name.replace(SPECIAL_CHARS_REGEXP, function(_: string, separator: string, letter: string, offset: string) {
+    return offset ? letter.toUpperCase() : letter;
+  }).replace(MOZ_HACK_REGEXP, 'Moz$1');
+};
+
+/* istanbul ignore next */
+export const on = function(element: HTMLElement, event: string, handler: (this: HTMLElement, ev: any) => any) {
+  if (element && event && handler) {
+    element.addEventListener(event, handler, false);
+  }
+}
+
+/* istanbul ignore next */
+export const off = function(element: HTMLElement, event: string, handler: (this: HTMLElement, ev: any) => any) {
+  if (element && event) {
+    element.removeEventListener(event, handler, false);
+  }
+}
+
+/* istanbul ignore next */
+export const once = function(el: HTMLElement, event: any, fn: Function) {
+  let listener = function() {
+    if (fn) {
+      // @ts-ignore
+      fn.apply(this, arguments);
+    }
+    off(el, event, listener);
+  };
+  on(el, event, listener);
+};
+
+/* istanbul ignore next */
+export function hasClass(el: HTMLElement, cls: string) {
+  if (!el || !cls) return false;
+  if (cls.indexOf(' ') !== -1) throw new Error('className should not contain space.');
+  if (el.classList) {
+    return el.classList.contains(cls);
+  } else {
+    return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
+}
+
+/* istanbul ignore next */
+export function addClass(el: HTMLElement, cls: string) {
+  if (!el) return;
+  let curClass = el.className;
+  let classes = (cls || '').split(' ');
+
+  for (let i = 0, j = classes.length; i < j; i++) {
+    let clsName = classes[i];
+    if (!clsName) continue;
+
+    if (el.classList) {
+      el.classList.add(clsName);
+    } else if (!hasClass(el, clsName)) {
+      curClass += ' ' + clsName;
+    }
+  }
+  if (!el.classList) {
+    el.className = curClass;
+  }
+}
+
+/* istanbul ignore next */
+export function removeClass(el: HTMLElement, cls: string) {
+  if (!el || !cls) return;
+  let classes = cls.split(' ');
+  let curClass = ' ' + el.className + ' ';
+
+  for (let i = 0, j = classes.length; i < j; i++) {
+    let clsName = classes[i];
+    if (!clsName) continue;
+
+    if (el.classList) {
+      el.classList.remove(clsName);
+    } else if (hasClass(el, clsName)) {
+      curClass = curClass.replace(' ' + clsName + ' ', ' ');
+    }
+  }
+  if (!el.classList) {
+    el.className = trim(curClass);
+  }
+}
+
+/* istanbul ignore next */
+export const getStyle = function(element: HTMLElement, styleName: any) {
+  if (!element || !styleName) return null;
+  styleName = camelCase(styleName);
+  if (styleName === 'float') {
+    styleName = 'cssFloat';
+  }
+  try {
+    let computed = (document.defaultView as any).getComputedStyle(element, '');
+    return element.style[styleName] || computed ? computed[styleName] : null;
+  } catch (e) {
+    return element.style[styleName];
+  }
+}
+
+/* istanbul ignore next */
+export function setStyle(element: HTMLElement, styleName: string[] | string, value: string) {
+  if (!element || !styleName) return;
+
+  if (typeof styleName === 'object') {
+    for (let prop in styleName) {
+      if (styleName.hasOwnProperty(prop)) {
+        setStyle(element, prop, styleName[prop]);
+      }
+    }
+  } else {
+    styleName = camelCase(styleName);
+    (element.style as Record<string, any>)[styleName] = value;
+  }
+}
+
+export const isScroll = (el: HTMLElement, vertical: string) => {
+  const determinedDirection = vertical !== null || vertical !== undefined;
+  const overflow = determinedDirection
+    ? vertical
+      ? getStyle(el, 'overflow-y')
+      : getStyle(el, 'overflow-x')
+    : getStyle(el, 'overflow');
+
+  return overflow.match(/(scroll|auto)/);
+}
+
+export const getScrollContainer = (el: any, vertical: string) => {
+  let parent = el;
+  const rootDom: any[] = [window, document, document.documentElement]
+  while (parent) {
+    if (rootDom.includes(parent)) {
+      return window;
+    }
+    if (isScroll(parent, vertical)) {
+      return parent;
+    }
+    parent = parent.parentNode;
+  }
+
+  return parent;
+}
+
+export const isInContainer = (el: HTMLElement, container: HTMLElement) => {
+  if (!el || !container) return false;
+
+  const elRect = el.getBoundingClientRect();
+  let containerRect;
+
+  if ([window, document, document.documentElement, null, undefined].includes(container)) {
+    containerRect = {
+      top: 0,
+      right: window.innerWidth,
+      bottom: window.innerHeight,
+      left: 0
+    };
+  } else {
+    containerRect = container.getBoundingClientRect();
+  }
+
+  return elRect.top < containerRect.bottom &&
+    elRect.bottom > containerRect.top &&
+    elRect.right > containerRect.left &&
+    elRect.left < containerRect.right;
+}
